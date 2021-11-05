49b9502da0d9180161470ea358131740f739bd3d
David Basilio Espitalher <dbasilioesp@gmail.com>
2021-11-04 21:50:10 -0300

    feat: save access token in signed cookie

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const cookieParser = require('cookie-parser')
 const SpotifyService = require('./src/spotify-service')
+const isDev = process.env.NODE_ENV === 'development';
 
 const app = express()
 
 app.use(cors());
+app.use(cookieParser(process.env.SECRET))
+
+const COOKIE_OPTIONS = {
+    secure: !isDev, 
+    httpOnly: true,
+    sameSite: 'strict',
+    signed: true,
+}
 
 app.get('/', async (req, res) => {
     res.json({
@@ -17,8 +27,19 @@ app.get('/', async (req, res) => {
 
 app.get('/episodes', async (req, res) => {
     try {
-        const auth = await SpotifyService.auth()
-        const episodes = await SpotifyService.getEpisodes(auth.access_token);
+        let accessToken = req.signedCookies['token'];
+        
+        if (!accessToken) {
+            const auth = await SpotifyService.auth()
+            accessToken = auth.access_token;
+            
+            res.cookie('token', accessToken, {
+                ...COOKIE_OPTIONS,
+                maxAge: auth.expires_in,
+            })
+        } 
+        
+        const episodes = await SpotifyService.getEpisodes(accessToken);
         
         res.json(episodes)
     } catch (error) {
