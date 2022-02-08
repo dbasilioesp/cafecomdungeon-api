19ee8e2c5f4e60c2569cfcefd5f3b648498c8dc9
David Basilio E <dbasilioesp@gmail.com>
2022-02-07 21:38:37 -0300

    feat(episodes): filter and paginate list of episodes

--- src/app/episodes/routes.ts
+++ src/app/episodes/routes.ts
@@ -4,11 +4,32 @@ import { PrismaClient } from "@prisma/client";
 const router = Router()
 
 router.get('/', async (req, res) => {
+  const { q, page } = req.query;
+  const take = 20
+  const skip = page ? Number(page) * take : 0;
+
+  const options: any = {
+    take,
+    skip,
+    include: {
+      hosts: true,
+    },
+  }
+
+  if (q) {
+    options.where = {
+      description: {
+        contains: q,
+      }
+    }
+  }
+
   const prisma = new PrismaClient();
 
-  const allEpisodes = await prisma.episode.findMany()
+  const allEpisodes = await prisma.episode.findMany(options)
+  const total = await prisma.episode.count()
 
-  res.json(allEpisodes);
+  res.json({ data: allEpisodes, page, skip, query: q, size: allEpisodes.length, total });
 });
 
 export default router;
